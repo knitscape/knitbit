5de002a744d5dc605bbede541de2702f3303b939
branchwelder <htwiggsmith@gmail.com>
2026-04-14 11:21:54 -0400

    add basic example

---
+++ src/examples/basic-chart.js
@@ -0,0 +1,28 @@
+const unit = new Bimp(
+  9,
+  10,
+  [
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  ],
+  [
+    { color: "#fbacda", label: "miss" },
+    { color: "#08ccab", label: "front knit" },
+    { color: "#eb4034", label: "front tuck" },
+    { color: "#079e85", label: "back knit" },
+    { color: "#b03027", label: "back tuck" },
+    { color: "#fcff46", label: "ftb xfer" },
+    { color: "#afff46", label: "btf xfer" },
+    { color: "#d48cff", label: "front drop" },
+    { color: "#8050c0", label: "back drop" },
+    { color: "#1a1a1a", label: "empty" },
+  ],
+);
+
+return {
+  ops: unit,
+  yarnFeeder: new Array(h).fill(1),
+  palette: ["#7245b5"],
+};
